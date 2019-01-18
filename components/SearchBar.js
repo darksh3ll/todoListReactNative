6edc8cbfd796eb4aa262844e43f7a0c8b457fae1
stephane job <darksh3ll.js@gmail.com>
2019-01-18 20:53:02 +0100

    Création des composants

---
+++ components/SearchBar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {View,TextInput} from 'react-native'
+class SearchBar extends Component {
+    render() {
+        return (
+            <View>
+                <TextInput/>
+            </View>
+        );
+    }
+}
+
+export default SearchBar;
