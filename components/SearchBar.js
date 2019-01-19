f15ed87e410f5e5819f23a917f19c57a0e7314c0
darksh3ll <darksh3ll.js@gmail.com>
2019-01-19 13:34:50 +0100

    Import Badge bibliothéque

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
-import {View,TextInput} from 'react-native'
+import {View, TextInput, Button} from 'react-native'
+import {Badge} from 'react-native-elements';
+
 class SearchBar extends Component {
+
     render() {
         return (
-            <View>
-                <TextInput/>
+            <View style={{paddingTop: 40}}>
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(e) => this.props.onChange(e)}
+                    value={this.props.value}
+                    placeholder="Ajouter une tâche
+"
+
+                />
+                <View/>
+                <View style={{flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+                    <Button
+                        title="Ajouter une tâche"
+                        accessibilityLabel="Learn more about this purple button"
+                        onPress={this.props.add}
+                    />
+
+                    <Badge
+                        value={this.props.lengthArrTodos}
+                        textStyle={{color: 'orange'}}
+                    />
+                </View>
             </View>
         );
     }
